8bda25dcedb0aad6dce315ae7a16e22bf88da608
mrppdex <aspiela@gmail.com>
2024-05-20 21:25:52 +0100

    bugs, add arrows

--- header.R
+++ header.R
@@ -16,7 +16,12 @@ grid.newpage()
 breaks_widths <- c(0.2, 0.1, 0.1)
 
 # create header
-create_header <- function(breaks_widths) {
+create_header <- function(breaks_widths, labels=NULL) {
+  # stop when labels is not null and has different length than breaks_widths
+  if (!is.null(labels) && length(labels) != length(breaks_widths)) {
+    stop("Length of labels must be the same as length of breaks_widths")
+  }
+
   breaks <- c(cumsum(breaks_widths))
   
   # use grid to plot a rectangle with specified margins
@@ -34,16 +39,15 @@ create_header <- function(breaks_widths) {
                gp = gpar(col = "black"))
   }
   
-  actual_breaks <- c(0, 
-                     breaks, 
-                     1)
-  # add a label to each of the rectangle segments
-  for (i in 1:(length(actual_breaks) - 1)) {
-    cat("Segment", i, "starts at", actual_breaks[i], "and ends at", actual_breaks[i+1], "\n")
-    grid.text(label = paste("Segment", i), 
-              x = PAGE_LEFT_MARGIN+HEADER_WIDTH*(actual_breaks[i] + actual_breaks[i+1])/2, 
-              y = 1 - PAGE_TOP_MARGIN - HEADER_HEIGHT/2,
-              just = c('center', 'center'))
+  actual_breaks <- c(0, breaks, 1)
+  # add labels to the segments
+  if (!is.null(labels)) {
+    for (i in 1:(length(actual_breaks) - 2)) {
+      grid.text(label = labels[i], 
+                x = PAGE_LEFT_MARGIN+HEADER_WIDTH*(actual_breaks[i] + actual_breaks[i+1])/2, 
+                y = 1 - PAGE_TOP_MARGIN - HEADER_HEIGHT/2,
+                just = c('center', 'center'))
+    }
   }
 
   # return all used parameters including margins
@@ -56,7 +60,7 @@ create_header <- function(breaks_widths) {
                 breaks = actual_breaks))
 }
 
-add_benefit_arrows <- function(obj, neutral_relative_x, direction) {
+add_benefit_arrows <- function(obj, neutral_relative_x, direction='up') {
     breaks <- obj$breaks
 
     # last two elements of breaks
@@ -81,11 +85,22 @@ add_benefit_arrows <- function(obj, neutral_relative_x, direction) {
                arrow=arrow(type="closed", length=unit(0.1, "inches")), 
                gp = gpar(fill =  "#043099", col = "#043099")) 
 
+    # draw text above the arrow
+    grid.text(label = ifelse(direction=='up', 'Prefers LY', 'Prefers Placebo'), 
+              x = x + distance_to_breaks/2, 
+              y = 1 - obj$PAGE_TOP_MARGIN - obj$HEADER_HEIGHT*0.5,
+              just = c('center', 'center'))
+
     grid.lines(x = c(x - 0.01, x - distance_to_breaks),
                 y = c(1 - obj$PAGE_TOP_MARGIN - obj$HEADER_HEIGHT*0.9, 1 - obj$PAGE_TOP_MARGIN - obj$HEADER_HEIGHT*0.9),
                 arrow=arrow(type="closed", length=unit(0.1, "inches")), 
                 gp = gpar(fill = "#043099", col = "#043099")) 
+
+    grid.text(label = ifelse(direction=='up', 'Prefers Placebo', 'Prefers LY'),
+              x = x - distance_to_breaks/2, 
+              y = 1 - obj$PAGE_TOP_MARGIN - obj$HEADER_HEIGHT*0.5,
+              just = c('center', 'center'))
 }
 
-my_obj <- create_header(breaks_widths)
-add_benefit_arrows(my_obj, 0.7, 1)
+my_header <- create_header(breaks_widths, c('Endpoint', 'Treatment', 'Dose'))
+add_benefit_arrows(my_header, 0.7, 'up')
