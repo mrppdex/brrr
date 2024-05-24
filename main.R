25b079e0c144c11eb98a3bf29d0cb7fee68b32f1
mrppdex <aspiela@gmail.com>
2024-05-24 08:27:01 +0100

    Refactor main.R to improve code structure and add forest plot simulation

--- main.R
+++ main.R
@@ -3,31 +3,52 @@ grid.newpage()
 # create header
 breaks_widths <- c(0.2, 0.1, 0.1)
 my_header <- create_header(breaks_widths, c('Endpoint', 'Treatment', 'Placebo'))
-#add_benefit_arrows(my_header, 0.7, 'up')
 
-current_y <- 1 - (HEADER_HEIGHT + PAGE_TOP_MARGIN)
+# function to generate a header, add benefit arrows and plot and boxex.
+# The function calculates the bottom edge of all plotted elements and
+# returns it as a y coordinate. This way, the next element can be plotted
+# below the previous one. Also, one of the arguments is the spacing
+# between the elements. This way, the function can be used to plot.
 
-global_box_x <- rev(my_header$breaks_positions)[2]
-global_box_width <- rev(my_header$breaks_positions)[1] - global_box_x
+add_benefits_box <- function(header, spacing, n_categories, single_category_height, 
+                             neutral_pos, n_ticks, from, to, label=NULL, logscale=FALSE, b=10, 
+                             show_axis=TRUE, direction='up') {
+    current_y <- 1 - (header$HEADER_HEIGHT + header$PAGE_TOP_MARGIN) - spacing
 
-# create a box
-box1 <- plot_box(global_box_x, current_y, global_box_width, 3, 0.05, 3, 5, 1, -2, expression(x[1]^2), FALSE)
+    global_box_x <- rev(header$breaks_positions)[2]
+    global_box_width <- rev(header$breaks_positions)[1] - global_box_x
 
-# add benefit arrows
-add_benefit_arrows(my_header, 3/5)
+    # create a box
+    box1 <- plot_box(global_box_x, current_y, global_box_width, n_categories, single_category_height, 
+                     neutral_pos, n_ticks, from, to, label, logscale, b, show_axis)
 
-from_ <- min(box1$axis$from, box1$axis$to)
-to_ <- max(box1$axis$from, box1$axis$to)
+    # add benefit arrows
+    add_benefit_arrows(header, neutral_pos/n_ticks, direction=direction)
 
-x_lower <- runif(3, from_, to_)
-x_upper <- runif(3, x_lower, to_)
+    return(list(box = box1, y_pos = min(box1$y_pos)))
+}
+
 
 # test the function
-plot_horizontal_seg(box1, c(x_lower[1], x_upper[1]), 1, col='purple')
-plot_horizontal_seg(box1, c(x_lower[2], x_upper[2]), 2, col='purple')
-plot_horizontal_seg(box1, c(x_lower[3], x_upper[3]), 3, col='purple')
+box1_data <- add_benefits_box(my_header, 0, 3, 0.05, 3, 5, 0, 1, expression(x[1]^2), 
+                              FALSE, direction='down')
+box1 <- box1_data$box
 
-# add dot
-plot_dot(box1, runif(1, x_lower[1], x_upper[1]), 1, col='purple')
-plot_dot(box1, runif(1, x_lower[2], x_upper[2]), 2, col='purple')
-plot_dot(box1, runif(1, x_lower[3], x_upper[3]), 3, col='purple')
+# function to simulate and plot forest tree
+
+simulate_forest_plot <- function(box, N1=1, col='purple') {
+
+    from_ <- min(box$axis$from, box$axis$to)
+    to_ <- max(box$axis$from, box$axis$to)
+
+    for (i in 1:(length(box$y_pos-1))) {
+        for (j in 1:N1) {
+            x_lower <- runif(1, from_, to_)
+            x_upper <- runif(1, x_lower, to_)
+
+            plot_forest_tree(box, x_lower, x_upper, runif(1, x_lower, x_upper), i, j, N1, col=col)
+        }
+    }
+}
+
+simulate_forest_plot(box1, 3, col='purple')
