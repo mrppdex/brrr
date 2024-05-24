5cfbc503493b795cc06f6c1413c9938e4440c3e1
Pawel Piela <aspiela@gmail.com>
2024-05-24 10:25:44 +0100

    Refactor main.R to improve code structure and add forest plot simulation

--- main.R
+++ main.R
@@ -10,10 +10,23 @@ my_header <- create_header(breaks_widths, c('Endpoint', 'Treatment', 'Placebo'))
 # below the previous one. Also, one of the arguments is the spacing
 # between the elements. This way, the function can be used to plot.
 
-add_benefits_box <- function(header, spacing, n_categories, single_category_height, 
+add_benefits_box <- function(obj, spacing, n_categories, single_category_height, 
                              neutral_pos, n_ticks, from, to, label=NULL, logscale=FALSE, b=10, 
                              show_axis=TRUE, direction='up') {
-    current_y <- 1 - (header$HEADER_HEIGHT + header$PAGE_TOP_MARGIN) - spacing
+    current_y <- NULL
+    if(!is.null(obj$name) & obj$name == 'header') {
+        header <- obj
+        current_y <- 1 - (header$HEADER_HEIGHT + header$PAGE_TOP_MARGIN) - spacing
+        
+        # add benefit arrows
+        add_benefit_arrows(header, neutral_pos/n_ticks, direction=direction)
+
+    } else if (!is.null(obj$name) & obj$name == 'box') {
+        header <- obj$header
+        current_y <- obj$y_pos - spacing
+    } else {
+        stop('The object is not a header')
+    } 
 
     global_box_x <- rev(header$breaks_positions)[2]
     global_box_width <- rev(header$breaks_positions)[1] - global_box_x
@@ -22,17 +35,63 @@ add_benefits_box <- function(header, spacing, n_categories, single_category_heig
     box1 <- plot_box(global_box_x, current_y, global_box_width, n_categories, single_category_height, 
                      neutral_pos, n_ticks, from, to, label, logscale, b, show_axis)
 
-    # add benefit arrows
-    add_benefit_arrows(header, neutral_pos/n_ticks, direction=direction)
 
-    return(list(box = box1, y_pos = min(box1$y_pos)))
+    # read header$breaks_position discard two last elements
+    # and calculate the mean of each pair of elements
+
+    breaks_positions <- header$breaks_positions
+    breaks_positions <- breaks_positions[1:(length(breaks_positions)-1)]
+    breaks_positions <- breaks_positions[-length(breaks_positions)] + diff(breaks_positions)/2
+
+    # create a function with arguments level=1 to the length of breaks_positions
+    # that gets the x position of the breaks_positions, and y in similar manner to
+    # yvec <- box$y_pos[n1] + (box$y_pos[n1+1] - box$y_pos[n1])*n2/(N2+1)
+    # and plots a text at that position
+
+    add_label <- function(label, leveln, subleveln, n=1, N=1) {
+        xvec <- breaks_positions[leveln]
+        yvec <- box1$y_pos[subleveln] + (box1$y_pos[subleveln+1] - box1$y_pos[subleveln])*n/(N+1)
+
+        grid.text(label = label, 
+                  x = unit(xvec, 'npc'), y = unit(yvec, 'npc'), just = c('center', 'center'))
+    }
+
+    return(list(label_fun=add_label, box = box1, name='box', header=header, y_pos = min(box1$y_pos)))
 }
 
 
 # test the function
 box1_data <- add_benefits_box(my_header, 0, 3, 0.05, 3, 5, 0, 1, expression(x[1]^2), 
                               FALSE, direction='down')
-box1 <- box1_data$box
+
+for (i in 1:3) {
+    box1_data$label_fun(sprintf('Efficacy endpoint %g', i), 1, i)
+    box1_data$label_fun('LY 10mg', 2, i, 1, 3)
+    box1_data$label_fun('LY 20mg', 2, i, 2, 3)
+    box1_data$label_fun('LY 40mg', 2, i, 3, 3)
+    box1_data$label_fun('Placebo', 3, i)
+}
+
+box2_data <- add_benefits_box(box1_data, 0.07, 3, 0.02, 3, 5, 0, 1, expression(x[1]^2), 
+                              FALSE, direction='down', show_axis=FALSE)
+
+# add labels to box2
+for (i in 1:3) {
+    box2_data$label_fun(sprintf('Efficacy endpoint 1%s', LETTERS[i]), 1, i)
+    box2_data$label_fun('LY 100mg', 2, i)
+    box2_data$label_fun('Placebo', 3, i)
+}
+
+box3_data <- add_benefits_box(box2_data, 0, 3, 0.04, 3, 5, 0, 1, "Efficacy endpoint", 
+                              FALSE, direction='down')
+
+# add labels to box3
+for (i in 1:3) {
+    box3_data$label_fun(sprintf('Efficacy endpoint 2%s', LETTERS[i]), 1, i)
+    box3_data$label_fun('LY 100mg', 2, i, 1, 2)
+    box3_data$label_fun('LY 200mg', 2, i, 2, 2)
+    box3_data$label_fun('Placebo', 3, i)
+}
 
 # function to simulate and plot forest tree
 
@@ -51,4 +110,6 @@ simulate_forest_plot <- function(box, N1=1, col='purple') {
     }
 }
 
-simulate_forest_plot(box1, 3, col='purple')
+simulate_forest_plot(box1_data$box, 3, col='purple')
+simulate_forest_plot(box2_data$box, 1, col='purple')
+simulate_forest_plot(box3_data$box, 2, col='forestgreen')
