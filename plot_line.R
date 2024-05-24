d0ebbf7aa452a56720856e35defeb814e5139359
mrppdex <aspiela@gmail.com>
2024-05-24 08:27:10 +0100

    Refactor plot_line.R to add plot_forest_tree function

--- plot_line.R
+++ plot_line.R
@@ -25,6 +25,14 @@ plot_dot <- function(box, x, n1, n2=1, N2=1, pch=19, size=unit(1, 'char'), col='
   
 }
 
+# using horizontal segment and dot, plot a forest plot
+plot_forest_tree <- function(box, x_lower, x_upper, x_dot, n1, n2=1, N2=1, col='purple') {
+  
+  plot_horizontal_seg(box, c(x_lower, x_upper), n1, n2, N2, col=col)
+  plot_dot(box, x_dot, n1, n2, N2, col=col)
+  
+}
+
 # test
 
 # grid.newpage()
