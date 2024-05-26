faff585dd8355a0aadc41852c6ef1ae4b159a93b
mrppdex <aspiela@gmail.com>
2024-05-26 15:54:19 +0100

    Refactor `test1.R` to update column name, add additional plot options, and improve code structure

--- R/test1.R
+++ R/test1.R
@@ -14,11 +14,12 @@ columns_specs <- c('Benefit'='endpoint',
               'Comparison\nHR or Odds Ratio\n(95% CI)'='col3')
 value_collapse <- c(TRUE, FALSE, FALSE, FALSE)
 
-split_axis_by_col <- 'estimator'
+split_axis_by_col <- 'axis_number'
+axes_labels_col <- 'estimator'
 split_box_by_col <- 'endpoint'
 
 part1_data <- plot_br(mock_data, columns_specs, breaks_widths, 
-                      split_axis_by_col, split_box_by_col, 
+                      split_axis_by_col, axes_labels_col, split_box_by_col, 
                       neutral_pos = 2, num_ticks = 6, 
                       top_margin=NULL, value_collapse=value_collapse)
 
@@ -35,7 +36,8 @@ columns_specs_risks <- c('Risk'='endpoint',
 
 value_collapse_risks <- c(FALSE, FALSE, FALSE, FALSE)
 
-split_axis_by_col <- 'estimator'
+split_axis_by_col <- 'axis_number'
+axes_labels_col <- 'estimator'
 split_box_by_col <- 'endpoint'
 
 risks_options <- page_options$new()
@@ -43,7 +45,7 @@ label_font_size <- risks_options$get_label_font_size()*1.2
 risks_options$set_label_font_size(label_font_size)
 
 part2_data <- plot_br(mock_data_risks, columns_specs_risks, breaks_width_risks,
-                      split_axis_by_col, split_box_by_col, userect=TRUE,
-                      neutral_pos = 2, num_ticks = 6, 
-                      top_margin=1-part1_data$last_y-0.05, value_collapse=value_collapse_risks,
+                      split_axis_by_col, axes_labels_col, split_box_by_col, 
+                      userect=TRUE, neutral_pos = 2, num_ticks = 6, 
+                      top_margin=1-part1_data$last_y+0.08, value_collapse=value_collapse_risks,
                       options_br=risks_options)
